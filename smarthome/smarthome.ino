d0e2af6b0b6dfa12a556154bb540b6d178a57af2
inicoding <94732319+inicoding@users.noreply.github.com>
2021-11-20 12:39:04 +0700

    Add files via upload

---
+++ smarthome/smarthome.ino
@@ -0,0 +1,38 @@
+int led = 13; 
+int led1 = 8;
+int sensor = 2;             
+int statusPir = LOW;         
+int data = 0;               
+int sensorInput = A1;
+
+
+void setup() {
+  pinMode(led, OUTPUT);    
+  pinMode(led1, OUTPUT); 
+  pinMode(sensor, INPUT);    
+  Serial.begin(9600);        
+}
+
+void loop(){
+  data = digitalRead(sensor); 
+  if ((data == HIGH) && (statusPir == LOW)) {           
+    digitalWrite(led, HIGH);
+      Serial.println("nyala"); 
+      statusPir = HIGH;       
+    } 
+  else {
+      if ((data == HIGH) && (statusPir == HIGH)) {
+         digitalWrite(led, LOW);
+      Serial.println("mati"); 
+      statusPir = LOW;
+      }
+  }
+ int lightLevel = analogRead(A0);
+
+  if (lightLevel > 230) {
+    digitalWrite(led1, LOW);
+  } else {
+    digitalWrite(led1, HIGH);
+  }
+  Serial.println(lightLevel);
+}
